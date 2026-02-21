1966830e9e0f556655f8e2284ff421305f68d468
Sam Pons de la Fuente <spons@Sams-MacBook-Air.local>
2026-02-21 11:24:54 +0100

    Add AuthProvider, BrowserRouter anf Theme from RadixUI

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './css/input.css'
+import './css/hamburgers.css'
+import 'animate.css'
+import "@radix-ui/themes/styles.css";
 import App from './App.jsx'
-import AuthProvider from './auth/AuthContext.jsx'
+import { AuthProvider } from './auth/AuthContext.jsx'
+import { Theme } from '@radix-ui/themes'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
       <BrowserRouter>
-        <App />
+        <Theme>
+          <App />
+        </Theme>
       </BrowserRouter>
     </AuthProvider>
   </StrictMode>,
