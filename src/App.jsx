b960047c45b17750010c66c461ee55a2d45754f5
Sam Pons de la Fuente <spons@Sams-MacBook-Air.local>
2026-02-23 00:50:50 +0100

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import AdminLayout from "./layouts/AdminLayout"
 import MainLayout from "./layouts/MainLayout"
 import Home from "./pages/Home"
 import Login from "./pages/auth/Login"
+import Register from "./pages/auth/Register"
+import ProtectRoute from "./auth/ProtectRoute"
+import UserDashboard from "./pages/UserDashboard"
+import AdminDashboard from "./pages/admin/AdminDashboard"
+import Vote from "./pages/Vote"
 
 export default function App() {
   return (
@@ -12,7 +17,35 @@ export default function App() {
           <Route path='/' element={<Home />}/>
         </Route>
 
-       
+        <Route element={<MainLayout />} >
+          <Route path="/login" element={<Login />} />
+        </Route>
+        <Route element={<MainLayout />} >
+          <Route path="/register" element={<Register />} />
+        </Route>
+        
+        <Route element={<MainLayout />} >
+          <Route path="/user" element={
+            <ProtectRoute >
+              <UserDashboard />
+            </ProtectRoute>
+          } />
+        </Route>
+
+        <Route element={<MainLayout />} >
+          <Route path="/votar" element={
+            <ProtectRoute >
+              <Vote />
+            </ProtectRoute>
+          } />
+        </Route>
+        
+        <Route path="/admin" element={
+          <ProtectRoute role="admin">
+            <AdminDashboard />
+          </ProtectRoute>
+        } />
+
       </Routes>
     </>
   )
