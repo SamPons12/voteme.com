3582d1e524e29b4ac741396841fb61c32889ac8e
Sam Pons de la Fuente <spons@Sams-MacBook-Air.local>
2026-03-07 20:38:37 +0100

    create function to check jwt expiration date and check if the user already voted

---
+++ src/helpers/userHelper.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { jwtDecode } from "jwt-decode";
+
+const instance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+
+export const isTokenExpired = (token) => {
+  if (!token) return true;
+  try {
+    const decodedToken = jwtDecode(token);
+    const currentTime = Date.now() / 1000;
+    return decodedToken.exp < currentTime;
+  } catch (err) {
+    console.log(err)
+    return true
+  }
+
+}
+
+export const hasUserVoted = async (token) => {
+  try {
+    instance.defaults.headers.authorization = `Bearer ${token}`
+    const response = await instance.post('/votes/me')
+    const data = await response.data;
+
+    if (!data.userVoted) {
+      return false
+    }
+
+    return true;
+  } catch (err) {
+    throw err
+  }
+}
