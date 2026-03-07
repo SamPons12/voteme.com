f7f368de0dcfe2e6a67461eaf85408e26f3baae9
Sam Pons de la Fuente <spons@Sams-MacBook-Air.local>
2026-03-07 20:36:17 +0100

    Check and handle jwt expiration date

--- src/auth/AuthContext.jsx
+++ src/auth/AuthContext.jsx
@@ -1,66 +1,74 @@
-import { createContext, useContext, useEffect, useState } from "react"
+import { createContext, useContext, useEffect, useState } from "react";
 import { loginUser, registerUser } from "../api/auth.service";
 import { jwtDecode } from "jwt-decode";
+import { isTokenExpired } from "../helpers/userHelper";
+import { useNavigate } from "react-router-dom";
 
-const AuthContext = createContext()
+const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
-
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
- 
+
+  const navigate = useNavigate();
+
   useEffect(() => {
-    const storedToken = localStorage.getItem('token')
+    const storedToken = localStorage.getItem("token");
     if (storedToken) {
-      const decode = jwtDecode(storedToken)
-      setUser(decode);
+      if (isTokenExpired(storedToken)) {
+        localStorage.removeItem("token");
+        navigate('/login', {replace: true})
+      } else {
+        const decode = jwtDecode(storedToken);
+        setUser(decode);
+      }
     }
     setLoading(false);
-  }, [])
+  }, []);
 
   const login = async (email, password) => {
     try {
-      setLoading(true)
+      setLoading(true);
       const data = await loginUser(email, password);
       if (data.token) {
-        localStorage.setItem('token', data.token);
-        const decode = jwtDecode(data.token)
+        localStorage.setItem("token", data.token);
+        const decode = jwtDecode(data.token);
         setUser(decode);
       }
       return data.token;
     } catch (err) {
-      throw err
+      throw err;
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
-  }
+  };
 
   const register = async (email, password) => {
     try {
       setLoading(true);
       const res = await registerUser(email, password);
-      return res
+      return res;
     } catch (err) {
-      throw err
+      throw err;
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
-  }
+  };
 
   const logout = () => {
-    localStorage.removeItem('token');
+    localStorage.removeItem("token");
     setUser(null);
-  }
+  };
 
   // TODO: Loading animation when loading true
 
   return (
     <div>
-      <AuthContext.Provider value={{user, login, register, logout, loading}}>
+      <AuthContext.Provider value={{ user, login, register, logout, loading }}>
         {children}
       </AuthContext.Provider>
     </div>
-  )
+  );
 }
 
 export function useAuth() {
