565ae52240937f17fe204384d691d6dbb104bf9b
Sam Pons de la Fuente <sampf2006@gmail.com>
2026-03-15 22:34:50 +0100

    Add toast component to admin layout to use sonner shacdn component

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -1,5 +1,6 @@
 import { AppSidebar } from "@/components/AppSidebar"
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { Toaster } from "@/components/ui/sonner"
 import { Outlet } from "react-router-dom"
 
 export default function AdminLayout() {
@@ -11,6 +12,7 @@ export default function AdminLayout() {
         <SidebarTrigger />
         <section className="px-10">
            <Outlet />
+           <Toaster />
         </section>
       </main>
     </SidebarProvider>
