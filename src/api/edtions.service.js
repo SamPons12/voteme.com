fe802791a3214585bfb143a0c28944821cd06bc7
Sam Pons de la Fuente <sampf2006@gmail.com>
2026-03-15 22:29:53 +0100

    Add udpate and delete function and edit catch handlers

--- src/api/edtions.service.js
+++ src/api/edtions.service.js
@@ -6,7 +6,7 @@ export const getAllEditions = async () => {
     const editions = await response.data;
     return editions;
   } catch (err) {
-    throw err
+    throw err.response
   }
 }
 
@@ -16,14 +16,26 @@ export const getActiveEditionCategories = async () => {
     const categories = await response.data;
     return categories;
   } catch (err) {
-    throw err
+    throw err.response
   }
 }
 
 export const deleteEdition = async (editionId) => {
   try {
-    
+    const response = await api.delete(`/editions/${editionId}`);
+    const result = await response.data;
+    return result;
   } catch (err) {
-    throw err
+    throw err.response
+  }
+}
+
+export const updateEdition = async (editionId, payload) => {
+  try {
+    const response = await api.put(`/editions/${editionId}`, payload);
+    const result = await response.data;
+    return result;
+  } catch (err) {
+    throw err.response
   }
 }
