f42fa0d1508597f257cd2280afb4b0232e1553e0
Sam Pons de la Fuente <sampf2006@gmail.com>
2026-03-23 13:41:26 +0100

    feat: add admin votes page

--- src/api/votes.service.js
+++ src/api/votes.service.js
@@ -1,5 +1,23 @@
 import { api } from "@/lib/axios-config";
 
+export async function getAllVotes() {
+  try {
+    const res = await api.get('/votes');
+    return res.data.votes;
+  } catch (err) {
+    throw err.response;
+  }
+}
+
+export async function getUserVotes() {
+  try {
+    const res = await api.get('/votes/mine');
+    return res.data.votes;
+  } catch (err) {
+    throw err.response;
+  }
+}
+
 export async function saveVotes(votes) {
   try {
     const res = api.post('/votes/save', {
