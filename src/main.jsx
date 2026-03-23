c1510197e95690a182723a052fe0fe2b128db77b
Sam Pons de la Fuente <sampf2006@gmail.com>
2026-03-23 13:41:10 +0100

    chore: update dependencies and add shadcn UI components

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './css/hamburgers.css'
 import 'animate.css'
 import "@radix-ui/themes/styles.css";
 import App from './App.jsx'
-import { AuthProvider } from './auth/AuthContext.jsx'
+import { AuthProvider } from './auth/AuthContext.jsx'
 import { Theme } from '@radix-ui/themes'
 
 createRoot(document.getElementById('root')).render(
